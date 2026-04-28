e853d0eb03f6b6e9266e7c66d417f8ae715e1626
Bryan Contador <180363173+BryanContador@users.noreply.github.com>
2026-04-28 09:45:12 -0600

    funcion

--- pene/app.py
+++ pene/app.py
@@ -4,3 +4,15 @@ saludo = "Hola, Mundo!"
 print(saludo)
 def saludar(nombre):
     return f"Hola, {nombre}!"
+
+
+#Llenar una funcion para sumar hasta 4 numeros
+def sumar(*numeros):
+    resultado = 0
+    for numero in numeros:
+        resultado += numero
+    return resultado
+#Ejemplo de uso de la funcion sumar
+print(sumar(1, 2))  # Salida: 3
+print(sumar(1, 2, 3))  # Salida: 6
+print(sumar(1, 2, 3, 4))  # Salida: 10  
